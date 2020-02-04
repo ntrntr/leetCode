b5c4e87be7884c86423506d8ef2540ca32987eb8
yzx <tianjiangziwu@gmail.com>
2020-02-04 17:33:49 +0800

    problems 508

---
+++ leetcode_python/problems/MostFrequentSubtreeSum508.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def findFrequentTreeSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_something1(self):
+        pass
+
+    def test_something2(self):
+        pass
+
+    def test_something3(self):
+        self.assertEqual(True, True)
+
+    def tearDown(self):
+        self.solution = None
+
+if __name__ == '__main__':
+    unittest.main()
