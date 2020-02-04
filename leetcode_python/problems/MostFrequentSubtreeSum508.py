c7aebfaf42b7459be2c1b5b7f4af7e7142b2ec78
yzx <tianjiangziwu@gmail.com>
2020-02-04 17:50:09 +0800

    problems 508 modify

--- leetcode_python/problems/MostFrequentSubtreeSum508.py
+++ leetcode_python/problems/MostFrequentSubtreeSum508.py
@@ -14,6 +14,29 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        ret = {}
+        root and self.getValue(root, ret)
+        current_count = 0
+        return_value = []
+        for k, v in ret.iteritems():
+            if v > current_count:
+                current_count = v
+                return_value = []
+                return_value.append(k)
+            elif v == current_count:
+                return_value.append(k)
+        return return_value
+
+    def getValue(self, root, ret):
+        leftValue = 0
+        rightValue = 0
+        if root.left:
+            leftValue = self.getValue(root.left, ret)
+        if root.right:
+            rightValue = self.getValue(root.right, ret)
+        total = root.val + leftValue + rightValue
+        ret[total] = ret.get(total, 0) + 1
+        return total
 
 
 class MyTestCase(unittest.TestCase):
@@ -22,7 +45,10 @@ class MyTestCase(unittest.TestCase):
         self.solution = Solution()
 
     def test_something1(self):
-        pass
+        node1 = TreeNode(5)
+        node1.left = TreeNode(2)
+        node1.right = TreeNode(-3)
+        self.assertEqual([-3,2,4], sorted(self.solution.findFrequentTreeSum(node1)))
 
     def test_something2(self):
         pass
