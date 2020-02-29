e99706343f80bb1010dced86044c7d31eef90ef7
yzx <tianjiangziwu@gmail.com>
2020-02-29 18:45:46 +0800

    problems 516

--- leetcode_python/problems/longestPalindromeSubseq516.py
+++ leetcode_python/problems/longestPalindromeSubseq516.py
@@ -2,12 +2,25 @@ import unittest
 
 
 class Solution(object):
+    def LCS(self, s1, s2):
+        s1_len = len(s1)
+        s2_len = len(s2)
+        state = [[0] * (s2_len + 1) for i in range(s1_len+1)]
+        for i in xrange(s1_len-1,-1,-1):
+            for j in xrange(s2_len-1,-1,-1):
+                if s1[i] == s2[j]:
+                    state[i][j] = state[i+1][j+1] + 1
+                else:
+                    state[i][j] = max(state[i+1][j], state[i][j+1])
+
+        return state[0][0]
+
     def longestPalindromeSubseq(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        return self.LCS(s, ''.join(reversed(s)))
 
 class MyTestCase(unittest.TestCase):
 
@@ -15,7 +28,7 @@ class MyTestCase(unittest.TestCase):
         self.solution = Solution()
 
     def test_something1(self):
-        pass
+        self.assertEqual(4, self.solution.longestPalindromeSubseq("bbbab"))
 
     def test_something2(self):
         pass
