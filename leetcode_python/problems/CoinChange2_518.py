9b1cdc5b029fedf0e1d30aef6fb2da18682fbf5d
yzx <tianjiangziwu@gmail.com>
2020-03-07 21:45:13 +0800

    problems 518

---
+++ leetcode_python/problems/CoinChange2_518.py
@@ -0,0 +1,53 @@
+import unittest
+
+
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        coins.sort()
+        while coins and coins[-1] > amount:
+            coins.pop()
+        coin_index = len(coins) - 1
+        return self.dfs(coin_index, amount, coins)
+
+    def dfs(self, coin_index, amount, coins):
+        ret = 0
+        if amount == 0:
+            return 1
+        if coin_index < 0:
+            return 0
+        if amount < 0:
+            return 0
+        current_value = coins[coin_index]
+        # use value
+        ret += self.dfs(coin_index, amount - current_value, coins)
+        # not use value
+        ret += self.dfs(coin_index - 1, amount, coins)
+        return ret
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_something1(self):
+        self.assertEqual(self.solution.change(5, [1,2,5]), 4)
+        self.assertEqual(self.solution.change(3, [2]), 0)
+        self.assertEqual(self.solution.change(10, [10]), 1)
+
+    def test_something2(self):
+        pass
+
+    def test_something3(self):
+        self.assertEqual(True, True)
+
+    def tearDown(self):
+        self.solution = None
+
+if __name__ == '__main__':
+    unittest.main()
