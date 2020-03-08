a79a9685ca64d43be413f5339989ebc5f5ba86a4
yzx <tianjiangziwu@gmail.com>
2020-03-08 17:43:22 +0800

    problems 521

---
+++ leetcode_python/problems/findLUSlength521.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something1(self):
+        solution = Solution()
+
+    def test_something2(self):
+        solution = Solution()
+
+    def test_something3(self):
+        self.assertEqual(True, True)
+
+    def tearDown(self):
+        self.solution = None
+
+if __name__ == '__main__':
+    unittest.main()
