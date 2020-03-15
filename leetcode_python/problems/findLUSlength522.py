d408777d2cdb712ee183606bf6daa5467e861398
yzx <tianjiangziwu@gmail.com>
2020-03-15 17:02:24 +0800

    problems 522

--- leetcode_python/problems/findLUSlength522.py
+++ leetcode_python/problems/findLUSlength522.py
@@ -1,5 +1,15 @@
 import unittest
+import functools
 
+def mycmp(left, right):
+    if right[0] == left[0]:
+        if right[1]  == left[1]:
+            return 0
+        elif right[1]  > left[1]:
+            return 1
+        else:
+            return -1
+    return right[0] - left[0]
 
 class Solution(object):
     def findLUSlength(self, strs):
@@ -7,31 +17,46 @@ class Solution(object):
         :type strs: List[str]
         :rtype: int
         """
-        candidate = []
-        max_len = 0
+        my_dict = {}
         for str in strs:
-            if len(str) > max_len:
-                max_len = len(str)
-                candidate = []
-                candidate.append(str)
-            elif len(str) == max_len:
-                candidate.append(str)
-        if len(candidate) == 1:
-            return len(candidate[0])
+            if not my_dict.has_key(str):
+                my_dict[str] = 0
+            my_dict[str] = my_dict[str] + 1
+        my_data = [(len(k), k) for k, v in my_dict.iteritems() if v <= 1]
+        my_data.sort(key=functools.cmp_to_key(mycmp))
+        if my_data:
+            for str_len, str in my_data:
+                if self.checkValid(str, my_dict):
+                    return str_len
+            return -1
         else:
-            my_dict = {}
-            for str in candidate:
-                if str in my_dict:
-                    my_dict[str] = my_dict[str] + 1
-                else:
-                    my_dict[str] = 1
-            if len(my_dict.keys()) == 1:
-                return -1
+            return -1
+
+    def checkValid(self, str, my_dict):
+        len_str = len(str)
+        candidates = [k for k,v in my_dict.iteritems() if len(k) >= len_str and k != str]
+        for candi in candidates:
+            if self.isSubsequence(str, candi):
+                return False
+        return True
+
+    def isSubsequence(self, sub_str, str):
+        str_len = len(str)
+        index = 0
+        for i in xrange(str_len):
+            if str[i] == sub_str[index]:
+                index = index + 1
+                if index >= len(sub_str):
+                    break
             else:
-                for k,v in my_dict.iteritems():
-                    if v == 1:
-                        return len(k)
-                return -1
+                continue
+        if index == len(sub_str):
+            return True
+        return False
+
+
+
+
 
 class MyTestCase(unittest.TestCase):
 
@@ -40,7 +65,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_something1(self):
         solution = Solution()
-        self.assertEqual(solution.findLUSlength(["aabbcc", "aabbcc","cb"]), 2)
+        # self.assertEqual(solution.findLUSlength(["aabbcc", "aabbcc","cb"]), 2)
+        self.assertEqual(solution.findLUSlength(["aaa","aaa","aa"]), -1)
+        # solution.findLUSlength(["aabbcc", "aabbcc", "cb"])
 
     def test_something2(self):
         solution = Solution()
