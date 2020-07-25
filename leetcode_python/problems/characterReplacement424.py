d851b0350ee65c520b9aa83dac91b92b5c2bec47
yzx <ntr.ntr@qq.com>
2020-07-25 12:24:39 +0800

    problem 424

--- leetcode_python/problems/characterReplacement424.py
+++ leetcode_python/problems/characterReplacement424.py
@@ -1,5 +1,5 @@
 import unittest
-
+import copy
 
 class Solution(object):
     def characterReplacement(self, s, k):
@@ -11,6 +11,35 @@ class Solution(object):
         s_len = len(s)
         if s_len <= k + 1:
             return s_len
+        ret = 1
+        test = {}
+        cache = [{}, ]
+        for index, c in enumerate(s):
+            test[c] = 1 + test.get(c, 0)
+            cache.append(copy.deepcopy(test))
+            while True:
+                next_expect_len = ret + 1
+                next_index = index + 1 - next_expect_len
+                if next_index >= 0:
+                    if self.checkValid(cache[next_index], cache[index+1], k):
+                        ret = next_expect_len
+                    else:
+                        cache[next_index] = {}
+                        break
+                else:
+                    break
+        return ret
+
+    def checkValid(self, start_dict, end_dict, kk):
+        largetst_count = 0
+        total_sum = 0
+        for k, v in end_dict.iteritems():
+            s_v = start_dict.get(k, 0)
+            count = v - s_v
+            total_sum += count
+            largetst_count = max(largetst_count, count)
+        left_not_same = total_sum - largetst_count
+        return left_not_same <= kk
 
 
 
@@ -19,8 +48,10 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
+
     def test_something1(self):
-        pass
+        # self.assertEqual(4, self.solution.characterReplacement("ABAB", 2))
+        self.assertEqual(4, self.solution.characterReplacement("AABABBA", 1))
 
     def test_something2(self):
         pass
