e068387667bad2e157abf7c488d4dc2e0e983969
yzx <ntr.ntr@qq.com>
2021-07-18 19:19:21 +0800

    493 2

--- leetcode_cpp/leetcode/reversePairs493.cpp
+++ leetcode_cpp/leetcode/reversePairs493.cpp
@@ -1,21 +1,45 @@
 #include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
+
+class Solution2 {
+public:
+    int reversePairs(vector<int>& nums) {
+        int ret = 0;
+        auto nums_size = nums.size();
+        vector<int> data;
+        data.push_back(nums[0]);
+        for(auto i = 1; i < nums_size; ++i)
+        {
+            auto it = upper_bound(data.begin(), data.end(), nums[i] * 2);
+            if (it != data.end())
+            {
+                ret += distance(it, data.end());
+            }
+            auto insert_it = lower_bound(data.begin(), data.end(), nums[i]);
+            data.insert(insert_it, nums[i]);
+        }
+        return ret;
+    }
+};
 
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
         int ret = 0;
         auto nums_size = nums.size();
-        for(auto i = 0; i < nums_size -1; ++i)
+        vector<long long int> data;
+        data.push_back(static_cast<long long int>(nums[0]));
+        for(auto i = 1; i < nums_size; ++i)
         {
-            auto current_value = static_cast<long long int>(nums[i]);
-            for(auto j = i + 1; j < nums_size; ++j)
+            auto it = upper_bound(data.begin(), data.end(), static_cast<long long int>(nums[i]) * 2);
+            if (it != data.end())
             {
-                if (current_value > 2 * static_cast<long long int>(nums[j]))
-                {
-                    ret += 1;
-                }
+                ret += distance(it, data.end());
             }
+            auto insert_it = lower_bound(data.begin(), data.end(), nums[i]);
+            data.insert(insert_it, static_cast<long long int>(nums[i]));
         }
         return ret;
     }
