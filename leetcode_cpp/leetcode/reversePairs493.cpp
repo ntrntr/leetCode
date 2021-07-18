469956083d6e8e3e1222ffc4059c2f86bc09b688
yzx <ntr.ntr@qq.com>
2021-07-18 21:34:52 +0800

    493 3

--- leetcode_cpp/leetcode/reversePairs493.cpp
+++ leetcode_cpp/leetcode/reversePairs493.cpp
@@ -45,6 +45,27 @@ public:
     }
 };
 
+class Solution3 {
+public:
+    int reversePairs(vector<int>& nums) {
+        int ret = 0;
+        auto nums_size = nums.size();
+        vector<long long int> data;
+        data.push_back(static_cast<long long int>(nums[0]));
+        for(auto i = 1; i < nums_size; ++i)
+        {
+            auto it = upper_bound(data.begin(), data.end(), static_cast<long long int>(nums[i]) * 2);
+            if (it != data.end())
+            {
+                ret += distance(it, data.end());
+            }
+            auto insert_it = lower_bound(data.begin(), data.end(), nums[i]);
+            data.insert(insert_it, static_cast<long long int>(nums[i]));
+        }
+        return ret;
+    }
+};
+
 int main(void)
 {
     return 0;
