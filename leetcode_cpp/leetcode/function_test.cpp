d5a7134fd343a8431b1a1b40deab52ff0f28495e
zhixiang.yang <zhixiang.yang@mihoyo.com>
2022-08-22 11:08:53 +0800

    2022.08.22

--- leetcode_cpp/leetcode/function_test.cpp
+++ leetcode_cpp/leetcode/function_test.cpp
@@ -3,9 +3,44 @@
 #include <algorithm>
 #include <functional>
 #include <tuple>
+#include <type_traits>
+#include <typeinfo>
+#ifndef _MSC_VER
+#   include <cxxabi.h>
+#endif
+#include <memory>
+#include <string>
+#include <cstdlib>
 
 #include<vector>
 using namespace std;
+
+template <class T>
+std::string
+type_name()
+{
+    using TR = std::remove_reference<T>::type;
+    std::unique_ptr<char, void(*)(void*)> own
+           (
+#ifndef _MSC_VER
+                abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                           nullptr, nullptr),
+#else
+                nullptr,
+#endif
+                std::free
+           );
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
 
 void function_test()
 {
@@ -87,15 +122,42 @@ public:
 
 void test_r_value_reference()
 {
-    TestA a = TestA();
-    
+    // TestA a = TestA();
+    int x = 10;
+    auto&& var1 = x;
+    cout << type_name<decltype(var1)>() << endl;
+    cout << type_name<decltype(x)>() << endl;
+    cout << type_name<decltype((x))>() << endl;
 }
 
+void test_r_value2()
+{
+    int w1, w2;
+    auto&& c1 = w1;
+    decltype(w1)&& c2 = std::move(w2);
+    cout << type_name<decltype(c2)>() << endl;
+    cout << type_name<decltype(c1)>() << endl;
+
+}
+
+template<typename T>
+void Func(T&& t)
+{
+    cout << type_name<T>() << endl;
+}
+class Foo{};
 int main(void)
 {
     cout << "hello world" << endl;
-    function_test();
-    test_lambda();
-    test_r_value_reference();
+    // function_test();
+    // test_lambda();
+    // test_r_value2();
+    
+    // string test = "Test";
+    // Func(test);
+    // Func(move(test));
+    
+    // class Foo {};
+    cout << typeid(Foo*[10]).name() << endl;
     return 0;
 }
